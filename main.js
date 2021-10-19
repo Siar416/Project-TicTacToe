f756fc4f866a22dd876ea4896c51a223bf0423b5
Siar <siar.wahidi@gmail.com>
2021-10-19 18:26:33 -0400

    Created function to add X when player selects a spot on gameboard. In beginning phase, will likely reimplement

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
+const GAMEBOARD = document.getElementById('gameBoard');
+const GRIDAREA = document.querySelectorAll('.gridArea');
+
+
+
+
 const GameBoardObj = {
     gamebord: ['X', 'O'],
+    player: function(name) {
+        const getName = () => name;
+
+        return { getName }
+    },
+
 };
 
 
 
-
+for (let i = 0; i < GRIDAREA.length; i++) {
+    GRIDAREA[i].addEventListener('click', () => {
+        if(GRIDAREA[i].textContent === 'X') {
+            console.log('Spot Taken');
+        } else {
+            let playerSelection = document.createElement('p');
+            playerSelection.textContent = 'X';
+            GRIDAREA[i].appendChild(playerSelection);
+        }
+    });
+}
 
