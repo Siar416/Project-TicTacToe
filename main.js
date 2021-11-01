ecceb8026442467a23e5d3f3f6776380df7b7a1d
Siar <siar.wahidi@gmail.com>
2021-11-01 18:21:48 -0400

    adding function to switch between players

--- main.js
+++ main.js
@@ -11,26 +11,46 @@ const player2 = Player('Zinny', 'O', 0)
 
 
 const GameBoardObj = {
-    gameboard: ['X', 'O']
+    gameboard: []
 };
 
 
+function turns() {
+    if(currentPlayer === player1) {
+        currentPlayer = player2;
+    } else {
+        currentPlayer = player1;
+    }
+}
+
+let currentPlayer = player1;
+
+
 function game() {
-    for(let i = 0; i < GRIDAREA.length; i++) {
+    for (let i = 0; i < GRIDAREA.length; i++) {
         GRIDAREA[i].addEventListener('click', () => {
-           if(GRIDAREA[i].textContent === player1.selection || GRIDAREA[i].textContent === player2.selection) {
-                alert('Spot taken!');
+            if(GRIDAREA[i].textContent === player1.selection || GRIDAREA[i].textContent === player2.selection) {
+                alert('Sorry, spot taken');
             } else if(GRIDAREA[i].textContent === '') {
-                //let selected = document.createElement('p');
-                //selected.textContent = player1.selection;
-                //player1.moves++;
-                //GRIDAREA[i].appendChild(selected);
-                GRIDAREA[i].textContent = player1.selection;
+                GRIDAREA[i].textContent = currentPlayer.selection;
+                //GRIDAREA[i].textContent = player1.selection;
+                GameBoardObj.gameboard.push(player1.selection);
+                player1.moves++;
+                turns();
+            } else {
+                GRIDAREA[i].textContent = currentPlayer.selection;
+                //GRIDAREA[i].textContent = player2.selection;
+                GameBoardObj.gameboard.push(player2.selection);
+                player2.moves;
+                turns();
+                
             }
         });
     }
 }
 game();
+
+
 
 
 
