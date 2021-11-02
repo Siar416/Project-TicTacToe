44979444f2feb668e5383ba24f51d8e3557180f6
Siar <siar.wahidi@gmail.com>
2021-11-02 19:14:34 -0400

    Added winning combo array. Working on checking creating function to check for winning combo also

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const GameBoardObj = {
 };
 
 
+
 function turns() {
     if(currentPlayer === player1) {
         currentPlayer = player2;
@@ -25,6 +26,17 @@ function turns() {
 
 let currentPlayer = player1;
 
+const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 6],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+];
+
 
 function game() {
     for (let i = 0; i < GRIDAREA.length; i++) {
@@ -34,23 +46,27 @@ function game() {
             } else if(GRIDAREA[i].textContent === '') {
                 GRIDAREA[i].textContent = currentPlayer.selection;
                 //GRIDAREA[i].textContent = player1.selection;
-                GameBoardObj.gameboard.push(player1.selection);
-                player1.moves++;
+                GameBoardObj.gameboard.push(currentPlayer.selection);
+                currentPlayer.moves++;
                 turns();
             } else {
-                GRIDAREA[i].textContent = currentPlayer.selection;
+                GRIDAREA[i].textContent = player2.selection;
                 //GRIDAREA[i].textContent = player2.selection;
                 GameBoardObj.gameboard.push(player2.selection);
-                player2.moves;
-                turns();
-                
+                currentPlayer.moves++;
             }
         });
     }
 }
 game();
 
-
+function winCombo() {
+    for(let i = 0; i < winningCombos.length; i++) {
+       if(winningCombos[i] === GameBoardObj.gameboard) {
+           console.log('winner');
+       }
+    }
+}
 
 
 
