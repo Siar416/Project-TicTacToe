3ca5df70c6759a9056c3a17d1c32680ec6f46089
Siar <siar.wahidi@gmail.com>
2021-11-14 14:46:11 -0500

    Made adjustments to the reset button. Replaced console.log statements by creating a div and display winner on screeen. Minor CSS changes to fix size

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// Globals
 const GAMEBOARD = document.getElementById('gameBoard');
 const CELL = document.querySelectorAll('[data-index]');
 const boxes = Array.from(document.getElementsByClassName('gridArea'));
 const restartBTN = document.getElementById('restartBTN');
 
+// player factory function
 const Player = (name, selection, moves) => {
     return { name, selection, moves }
 }
+
 
 const player1 = Player('Siar', 'X', 0);
 const player2 = Player('Zinny', 'O', 0)
@@ -15,7 +18,7 @@ const GameBoardObj = {
     gameboard: [null, null, null, null, null, null, null, null, null]
 };
 
-
+// switching betweens
 let currentPlayer = player1;
 function turns() {
     if(currentPlayer === player1) {
@@ -25,7 +28,7 @@ function turns() {
     }
 }
 
-
+// different win combos 
 const winningCombos = [
         [0, 1, 2],
         [3, 4, 5],
@@ -65,55 +68,56 @@ game();
 
 
 function winCheck() {
+    let result = document.getElementById('result');
     if(CELL[0].textContent === currentPlayer.selection) {
         if(CELL[1].textContent === currentPlayer.selection && CELL[2].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
         if(CELL[3].textContent === currentPlayer.selection && CELL[6].textContent === currentPlayer.selection) {
-        console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
         if(CELL[4].textContent === currentPlayer.selection && CELL[8].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
     }
     if(CELL[8].textContent === currentPlayer.selection) {
         if(CELL[2].textContent === currentPlayer.selection && CELL[5].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
         if(CELL[6].textContent === currentPlayer.selection && CELL[7].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
     }
     if(CELL[4].textContent === currentPlayer.selection) {
         if(CELL[1].textContent === currentPlayer.selection && CELL[7].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
         if(CELL[3].textContent === currentPlayer.selection && CELL[5].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
             return true;
         }
         if(CELL[2].textContent === currentPlayer.selection && CELL[6].textContent === currentPlayer.selection) {
-            console.log(`${currentPlayer.selection} wins`);
+            result.textContent = `${currentPlayer.selection} wins`;
         }
     }
 }
 
-
+// checks for draw game
 function drawCheck() {
     if(currentPlayer.moves === 5) {
-        console.log('draw');
+        result.textContent = 'Draw';
         return;
     }
 }
 
-
-const drawBoard = () => {
+// rendering the board
+    (function drawBoard() {
     boxes.forEach((box, index) => {
         let styleString = [];
         if(index < 3) {
@@ -130,15 +134,19 @@ const drawBoard = () => {
         }
         box.style = styleString;
     });
-}
-drawBoard();
+})();
 
+//resetting everything
 restartBTN.addEventListener('click', () => {
     CELL.forEach((cell, index) => {
         cell[index] = null;
+        player1.moves = 0;
+        player2.moves = 0;
+        result.textContent = "let's Play";
     })
     boxes.forEach(box => {
         box.innerHTML = '';
+        GameBoardObj.gameboard = [null, null, null, null, null, null, null, null, null];
         currentPlayer = player1;
     });
 });
